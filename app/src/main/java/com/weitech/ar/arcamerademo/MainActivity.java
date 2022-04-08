8c4389a9739bda3005f5f510ddc331736d1ea133
innerpeacer <innerpeacer@gmail.com>
2022-04-08 16:10:40 +0800

    Show Unity Window

--- app/src/main/java/com/weitech/ar/arcamerademo/MainActivity.java
+++ app/src/main/java/com/weitech/ar/arcamerademo/MainActivity.java
@@ -1,8 +1,12 @@
 package com.weitech.ar.arcamerademo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initButtons();
     }
+
+    private void initButtons() {
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.main);
+        {
+            Button button = new Button(this);
+            button.setText("Show Unity");
+            button.setX(200);
+            button.setY(500);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    startUnity();
+                }
+            });
+            layout.addView(button);
+        }
+    }
+
+    private void startUnity() {
+        Intent intent = new Intent(this, TestUnityActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/weitech/ar/arcamerademo/TestUnityActivity.java
@@ -0,0 +1,12 @@
+package com.weitech.ar.arcamerademo;
+
+import android.os.Bundle;
+
+import com.unity3d.player.UnityPlayerActivity;
+
+public class TestUnityActivity extends UnityPlayerActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
