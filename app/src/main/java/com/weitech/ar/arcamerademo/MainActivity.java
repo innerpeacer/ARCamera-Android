57e856597fd671b3da3233760a80f13b795f021c
innerpeacer <innerpeacer@gmail.com>
2022-05-16 23:50:32 +0800

    Testing ARCore Support

--- app/src/main/java/com/weitech/ar/arcamerademo/MainActivity.java
+++ app/src/main/java/com/weitech/ar/arcamerademo/MainActivity.java
@@ -2,12 +2,18 @@ package com.weitech.ar.arcamerademo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Session;
+import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,10 +60,57 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private boolean checkARSupport() throws Exception {
+        Log.i(TAG, "---checkARSupport---");
+        ArCoreApk.Availability isArSupported = ArCoreApk.getInstance().checkAvailability(this);
+        Log.i(TAG, "Availability: " + isArSupported);
+        if (isArSupported == ArCoreApk.Availability.SUPPORTED_INSTALLED) {
+            try {
+                new Session(this);
+            } catch (UnavailableDeviceNotCompatibleException e) {
+                Log.i(TAG, "UnavailableDeviceNotCompatibleException");
+                return false;
+            }
+            return true;
+        } else if (isArSupported == ArCoreApk.Availability.SUPPORTED_NOT_INSTALLED) {
+            Log.i(TAG, "Not Installed!!!!");
+            ArCoreApk.InstallStatus status = ArCoreApk.getInstance().requestInstall(this, true);
+            Log.i(TAG, "Status: " + status);
+            return false;
+        } else if (isArSupported == ArCoreApk.Availability.SUPPORTED_APK_TOO_OLD) {
+            Log.i(TAG, "ApkTooOld!!!!");
+            ArCoreApk.InstallStatus status = ArCoreApk.getInstance().requestInstall(this, true);
+            Log.i(TAG, "Status: " + status);
+            if (status == ArCoreApk.InstallStatus.INSTALLED) {
+                try {
+                    new Session(this);
+                } catch (UnavailableDeviceNotCompatibleException e) {
+                    Log.i(TAG, "UnavailableDeviceNotCompatibleException");
+                    return false;
+                }
+                return true;
+            } else if (status == ArCoreApk.InstallStatus.INSTALL_REQUESTED) {
+                return false;
+            }
+        }
+        return false;
+    }
+
     private void startCameraUnity() {
-        Intent intent = new Intent(this, ARCameraActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-        startActivity(intent);
+        boolean isSupported = false;
+        try {
+            isSupported = checkARSupport();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (isSupported) {
+            Intent intent = new Intent(this, ARCameraActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(intent);
+        } else {
+            Log.i(TAG, "Not Supported, Ignore");
+        }
     }
 
     private void startPreviewUnity() {
